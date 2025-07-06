00025d17183350369e796f0ea804ebf3671fa251
esrafil <elahi.esrafil@okala.com>
2025-07-06 21:40:19 +0330

    init

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <span>index page</span>
+  </div>
+</template>
---
+++ pages/works/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <span>works page</span>
+  </div>
+</template>
